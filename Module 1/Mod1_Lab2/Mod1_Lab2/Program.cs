16f235f62494d05b99d3dc68793651bd3db2cab7
Thomas Aschemann <taschemann@siu.edu>
2018-01-29 14:49:00 -0600

    Commit for Module 1 challenge (Lab 2)

---
+++ Module 1/Mod1_Lab2/Mod1_Lab2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Mod1_Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student and Teacher Information
+            string firstName = "";
+            string lastName = "";
+            DateTime birthDate;
+            string firstAddress = "";
+            string secondAddress = "";
+            string city = "";
+            string state = "";
+            int zipCode = 0;
+            string country = "";
+
+            //Program Information
+            string programName = "";
+            string departmentHead = "";
+            string degrees = "";
+
+            //Degree information
+            string degreeName = "";
+            int creditsRequired = 0;
+
+            //Course Information
+            string courseName = "";
+            int credits = 0;
+            int durationInWeeks = 0;
+            string teacher = "";
+
+        }
+    }
+}
