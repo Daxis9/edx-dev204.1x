a693694a66eb341dcdab4ae9cb1f21701c1d30a7
Thomas Aschemann <taschemann@siu.edu>
2018-01-30 12:30:30 -0600

    Completed Module 3 and finished course

---
+++ Module 3/Mod3_ChallengeLab/Mod3_ChallengeLab/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Mod3_ChallengeLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void GetStudentInformation()
+        {
+            Console.WriteLine("Enter the student's first name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter the student's last name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Getting student's birthday...");
+            DateTime birthday = DateTime.Now;
+        }
+
+        static void PrintStudentDetails(string first, string last, string birthday)
+        {
+            Console.WriteLine("{0} {1} was born on {3}", first, last, birthday);
+        }
+    }
+}
---
+++ Module 3/Mod3_Lab1/Mod3_Lab1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Mod3_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Sum(20, 40);
+            Console.WriteLine($"The sum of 20 and 40 is: {result}");   
+        }
+
+        static int Sum(int first, int second)
+        {
+            int sum = first + second;
+            //Console.WriteLine($"The sum of {first} and {second} is: {sum}");
+            return sum;
+        }
+
+        static int Sum(int first,  int second, int third)
+        {
+            int sum = first + second + third;
+            return sum;
+        }
+
+        static double Sum(double first, double second)
+        {
+            double result = first + second;
+            return result;
+        }
+    }
+}
