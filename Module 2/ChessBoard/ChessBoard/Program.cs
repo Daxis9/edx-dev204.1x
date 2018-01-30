9ece401be983f4384ae5e09a4dcedef3bc912295
Thomas Aschemann <taschemann@siu.edu>
2018-01-30 08:51:10 -0600

    Completion of Module 2

---
+++ Module 2/ChessBoard/ChessBoard/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ChessBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int col;
+            int row;
+            
+            for (col = 0; col < 8; col++)
+            {
+                for (row = 0; row < 8; row++)
+                {
+                    if (col % 2 == 0)
+                    {
+                        if(row % 2 == 0)
+                        {
+                            Console.Write("X");
+                        }
+                        else
+                        {
+                            Console.Write("O");
+                        }
+                    }
+                    else if(col % 2 == 1)
+                    {
+                        if(row % 2 == 0)
+                        {
+                            Console.Write("O");
+                        }
+                        else
+                        {
+                            Console.Write("X");
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- Module 2/Mod2_Lab1/Mod2_Lab1/Program.cs
+++ Module 2/Mod2_Lab1/Mod2_Lab1/Program.cs
@@ -6,7 +6,18 @@ namespace Mod2_Lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Check for even number
+            Console.WriteLine("Please enter an integer value and press Enter: ");
+            int input = Int32.Parse(Console.ReadLine());
+
+            if (input % 2 == 0)
+            {
+                Console.WriteLine("The entered number was an even number.");
+            }
+            else
+            {
+                Console.WriteLine("The entered number was not an even number.");
+            }
         }
     }
 }
